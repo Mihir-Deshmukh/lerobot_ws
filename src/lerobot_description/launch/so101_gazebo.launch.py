cf3afd2a187bc6736616f69cf4a105c74bcf370c
Mihir-Deshmukh <mihirdeshmukh15@gmail.com>
2025-08-26 21:17:20 -0400

    Added RGBD Camera

--- src/lerobot_description/launch/so101_gazebo.launch.py
+++ src/lerobot_description/launch/so101_gazebo.launch.py
@@ -63,6 +63,10 @@ def generate_launch_description():
         executable="parameter_bridge",
         arguments=[
             "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+            "/camera@sensor_msgs/msg/Image[gz.msgs.Image",
+            "/camera/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo",
+            "/camera_depth@sensor_msgs/msg/Image[gz.msgs.Image",
+            "/camera_depth/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo",
         ]
     )
 
